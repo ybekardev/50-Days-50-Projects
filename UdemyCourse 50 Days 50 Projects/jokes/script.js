7899d49dc57103014c5bde25219f19416ef27a3e
ybekardev <y_bekar@hotmail.com>
2021-02-02 22:19:20 -0600

    keyBoxes project added

--- UdemyCourse 50 Days 50 Projects/jokes/script.js
+++ UdemyCourse 50 Days 50 Projects/jokes/script.js
@@ -5,16 +5,34 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke();
 
-function generateJoke() {
+f// USING ASYNC/AWAIT
+async function generateJoke() {
     const config = {
-        headers: {
-            Accept: 'application/json'
-        },
+      headers: {
+        Accept: 'application/json',
+      },
     }
-    
-    fetch('https://icanhazdadjoke.com', config)
-    .then((response) => response.json())
-    .then((data) => {
-        jokeEl.innerHTML = data.joke
-    })
-}
+  
+    const res = await fetch('https://icanhazdadjoke.com', config)
+  
+    const data = await res.json()
+  
+    jokeEl.innerHTML = data.joke
+  }
+  
+  // USING .then()
+  // function generateJoke() {
+  //   const config = {
+  //     headers: {
+  //       Accept: 'application/json',
+  //     },
+  //   }
+  
+  //   fetch('https://icanhazdadjoke.com', config)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       jokeEl.innerHTML = data.joke
+  //     })
+  // }
+
+  
